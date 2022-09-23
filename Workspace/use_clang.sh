e355d92d240bc4a5c1061e3b1acfc3b8aa2b6610
AROBS-Arad <110459558+AROBS-Arad@users.noreply.github.com>
2022-09-23 21:51:25 +0300

    Add files via upload

---
+++ Workspace/use_clang.sh
@@ -0,0 +1,5 @@
+@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+update-alternatives --set cc /usr/bin/clang
+update-alternatives --set c++ /usr/bin/clang++
---
+++ Workspace/use_gcc.sh
@@ -0,0 +1,5 @@
+@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+update-alternatives --set cc /usr/bin/gcc
+update-alternatives --set c++ /usr/bin/g++
